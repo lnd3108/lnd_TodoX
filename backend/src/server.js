09aeb80cb54bf13ad2aeade2e6ee71168bb6e911
lnd3108 <luongphephe@gmail.com>
2025-11-21 16:40:36 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,32 @@ import tasksRouter from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/tasks", tasksRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+// Start the server after connecting to the database
 
 connectDB().then(() => {
   app.listen(PORT, () => {
